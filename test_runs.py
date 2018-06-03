277b62905327535633614c28fd0ef5f20c5a103d
skanda <skandakk@gmail.com>
2018-06-03 12:02:29 -0400

    adding config file, adding gitignore

--- test_runs.py
+++ test_runs.py
@@ -2,12 +2,16 @@ import sqlutilpy as sqlutil
 # import astrolibpy.utils.sqlutil as sqlutil
 
 def main():
+    user = "skanda_kaashyap"
+    # Whitespace does not matter
     ra, dec = sqlutil.get(
-        """ select ra, dec from des_dr1.main where q3c_radial_query(ra,dec,317.2044,-51.1656,1)""",
+        """select ra, dec from des_dr1.main where q3c_radial_query(ra,dec,317.2044,-51.1656,1)""",
         host="wsdb.hpc1.cs.cmu.edu", user="skanda_kaashyap", password="kZ5Wlo.s"
     )
 
+
     print("ra:", ra, "dec:", dec)
+    print("ra type:", type(ra), "dec type:", type(dec))
 
 if __name__ == "__main__":
     main()
