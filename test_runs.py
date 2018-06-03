6924ecc359fe3eb2681f4b143efd7178b4703255
skanda <skandakk@gmail.com>
2018-06-03 11:43:12 -0400

    getting data from database working

---
+++ test_runs.py
@@ -0,0 +1,13 @@
+# import sqlutil
+import astrolibpy.utils.sqlutil as sqlutil
+
+def main():
+    ra, dec = sqlutil.get(
+        """ select ra, dec from des_dr1.main where q3c_radial_query(ra,dec,317.2044,-51.1656,1)""",
+        host="wsdb.hpc1.cs.cmu.edu", user="skanda_kaashyap", password="kZ5Wlo.s"
+    )
+
+    print("ra:", ra, "dec:", dec)
+
+if __name__ == "__main__":
+    main()
