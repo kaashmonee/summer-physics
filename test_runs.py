1bdaa4e668643bc77145f8b0f9b57948c9b7dd56
skanda <skandakk@gmail.com>
2018-06-03 12:05:42 -0400

    further changes and added database file

--- test_runs.py
+++ test_runs.py
@@ -1,12 +1,15 @@
 import sqlutilpy as sqlutil
+import database-config
 # import astrolibpy.utils.sqlutil as sqlutil
 
 def main():
-    user = "skanda_kaashyap"
     # Whitespace does not matter
+    username = database-config.username
+    password = database-config.password
+
     ra, dec = sqlutil.get(
         """select ra, dec from des_dr1.main where q3c_radial_query(ra,dec,317.2044,-51.1656,1)""",
-        host="wsdb.hpc1.cs.cmu.edu", user="skanda_kaashyap", password="kZ5Wlo.s"
+        host="wsdb.hpc1.cs.cmu.edu", user=username, password=password
     )
 
 
